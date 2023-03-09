6fa9fe65c64bb62810cc081a9354440664e4556f
Hitesh Kumar Talhilyani <Hitesh.Talhilyani@emaratech.ae>
2023-03-09 16:18:30 +0400

    Different ways for print in Python

---
+++ PY_Basic_Day1/print.py
@@ -0,0 +1,33 @@
+## Learn Print statement
+## Attend Quiz on Udemy Skills Assessment 
+## String Manipulation 
+
+print("Hello World!\nHello world ")
+
+## Sting Concat 
+print ("Hello" +" "+ "Hitesh")
+print(2+3)  ## Retun sum of 2 +3 = 5 
+print('Sring Concatenation is done with "+" sign.')
+print('e.g.print("Hello + "World")')
+
+# To print below content using print function with line of code
+#I'm learning to become a Python developer!
+#I'm so excited!
+
+print ('I\'m learning to become a Python developer!\nI\'m so excited!')
+print ('Hello, World!')
+print ('Hello World!')
+print ("I'm studying python!!")
+print ('I\'m studying python!') 
+print ()
+print ('and having fun')
+print ('I\'m studying python!,and having fun')
+print ('I\'m studying python!\nand having fun')
+
+# Using sep to specify the separator character
+print("apple", "banana", "orange", sep=", ")
+
+
+# Using end to specify the end character this will print the statement in one line 
+print("Hello", end=" ")
+print("world!")
